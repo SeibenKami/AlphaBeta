3b8e52a10aeb69148c9209a518f090cb1ae6c0c7
Frank Adu <64962641+SeibenKami@users.noreply.github.com>
2022-06-22 01:29:38 +0000

    adding

---
+++ lens/lib/services/api_service.dart
@@ -0,0 +1,22 @@
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+
+import '../constants.dart';
+import '../models/user_model.dart';
+
+
+class ApiService {
+  Future<List<UserModel>?> getUsers() async {
+    try {
+      var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        List<UserModel> _model = userModelFromJson(response.body);
+        return _model;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+}
