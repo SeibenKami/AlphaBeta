b526c483bdff562d4eee10f1e39fa562e2b9ab1a
Oteng-Derrick <42379936+derrique12@users.noreply.github.com>
2022-06-22 16:07:06 +0000

    search page

--- lens/lib/screens/search_page/search_screen.dart
+++ lens/lib/screens/search_page/search_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:lens/screens/browser/browser.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key, required this.keyword}) : super(key: key);
@@ -10,37 +13,74 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  final GlobalKey<FormState> _searchFormKey = GlobalKey<FormState>();
   final TextEditingController searchController = TextEditingController();
+   Completer<WebViewController> webController = Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    searchController.text = widget.keyword;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.green,
+        elevation: 0,
+        centerTitle: true,
         leading: IconButton(
           onPressed: () {
             Navigator.pushReplacement(context,
                 MaterialPageRoute(builder: (context) => const Browser()));
           },
-          icon: const Icon(Icons.home),
+          icon: const Icon(
+            Icons.home,
+            size: 28,
+          ),
         ),
-        title: TextFormField(
-          controller: searchController,
-          decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              hintText: 'Search for web address',
-              suffixIcon: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.search,
-                  color: Colors.black,
-                ),
-              )),
-          onFieldSubmitted: (value) {},
+        leadingWidth: 12,
+        title: SizedBox(
+          height: 38,
+          child: TextFormField(
+            controller: searchController,
+            decoration: InputDecoration(
+                contentPadding: const EdgeInsets.only(left: 12),
+                fillColor: Colors.white,
+                filled: true,
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: const BorderSide(color: Colors.greenAccent)),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: const BorderSide(color: Colors.white)),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: const BorderSide(color: Colors.white)),
+                hintText: 'Search for web address',
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.bookmark_add_outlined,
+                    color: Colors.black,
+                    size: 20,
+                  ),
+                )),
+            onFieldSubmitted: (value) {
+              searchController.text=value;
+            },
+          ),
         ),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.menu))],
       ),
-      body: SizedBox(),
+      body: SafeArea(
+          child: WebView(
+        initialUrl: "https://google.com/search?q=${searchController.text}",
+        javascriptMode: JavascriptMode.unrestricted,
+         onWebViewCreated: (WebViewController webViewController) {
+           webController.complete(webViewController);
+        },
+              )),
     );
   }
 }
